9ba0cbaf852556db0b175e20a41511fe102340d0
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2023-05-13 14:47:37 +0200

    Footer upraven

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,41 @@
 <footer class="bg-dark py-1">
     <div class="container d-flex justify-content-center pb-1">
-        <a href="https://github.com/Karuta2000" class="btn btn-dark text-light btn-lg btn-floating mx-2">
+        <a href="https://github.com/Karuta2000/NeoNasa" class="btn btn-dark text-light btn-lg btn-floating mx-2">
             <i class="fa fa-github" aria-hidden="true"></i>
         </a>
     </div>
-    <hr class="my-2" style="background: linear-gradient(to right, #000000, #4d4d4d, #8c8c8c); border: none; height: 2px;">
-    <div class="container text-center">
-        <p class="text-white mb-0">Copyright &copy; 2023
-            <a href="#" class="text-white fw-bold">...</a>
+    <hr class="my-2"
+        style="background: linear-gradient(to right, #000000, #4d4d4d, #8c8c8c); border: none; height: 2px;">
+    <div class="container text-center mx-auto">
+        <div class="row w-100 text-white mx-auto">
+            <div class="col-4">
+
+                <ul class="list-group list-group-flushrounded p-2 shadow">
+                    <li class="list-group-item bg-dark text-white list-group-item-dark bg-gradient">View:</li>
+                    <li class="list-group-item list-group-item-dark">Tynk00</li>
+                    <li class="list-group-item list-group-item-dark">Smap01</li>
+                    <li class="list-group-item list-group-item-dark">---</li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <ul class="list-group list-group-flushrounded p-2 shadow">
+                    <li class="list-group-item bg-dark text-white list-group-item-dark bg-gradient">Model:</li>
+                    <li class="list-group-item list-group-item-dark">Tatv00</li>
+                    <li class="list-group-item list-group-item-dark">Vanm34</li>
+                    <li class="list-group-item list-group-item-dark">Mudp01</li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <ul class="list-group list-group-flushrounded p-2 shadow">
+                    <li class="list-group-item bg-dark text-white  list-group-item-dark bg-gradient">Controller:</li>
+                    <li class="list-group-item list-group-item-dark">Engt01</li>
+                    <li class="list-group-item list-group-item-dark">Jezf00</li>
+                    <li class="list-group-item list-group-item-dark">Birs03</li>
+                </ul>
+
+            </div>
+        </div>
+
         </p>
 
     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/256/1534/1534067.png">
     <title>NeoNASA</title>
 </head>
